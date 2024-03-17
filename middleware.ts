739bd5150307da3ee940f97a088d6bc77efb49b9
Netanel Gilad <netanelg@wix.com>
2024-03-17 09:28:02 +0200

    pass session cookie for first request as well

--- middleware.ts
+++ middleware.ts
@@ -4,14 +4,20 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
   const cookies = request.cookies;
-  const res = NextResponse.next();
   if (cookies.get(WIX_REFRESH_TOKEN_COOKIE)) {
-    return res;
+    return NextResponse.next();
   }
+
   const wixClient = createClient({
     auth: OAuthStrategy({ clientId: process.env.WIX_CLIENT_ID! })
   });
   const tokens = await wixClient.auth.generateVisitorTokens();
+
+  request.cookies.set(WIX_REFRESH_TOKEN_COOKIE, JSON.stringify(tokens.refreshToken));
+  const res = NextResponse.next({
+    request
+  });
+
   res.cookies.set(WIX_REFRESH_TOKEN_COOKIE, JSON.stringify(tokens.refreshToken), {
     maxAge: 60 * 60 * 24 * 30
   });
