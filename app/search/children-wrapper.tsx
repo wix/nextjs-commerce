911c4f91abb0bb2d00ed7446dcfd84e505b25b79
Arun Shankar Ezhilarasan <arun.ezhilarasan@betmakers.com>
2024-09-17 13:11:11 +0530

    fix search params issue

--- app/search/children-wrapper.tsx
+++ app/search/children-wrapper.tsx
@@ -6,5 +6,5 @@ import { Fragment } from 'react';
 // Ensure children are re-rendered when the search query changes
 export default function ChildrenWrapper({ children }: { children: React.ReactNode }) {
   const searchParams = useSearchParams();
-  return <Fragment key={searchParams.get('q')}>{children}</Fragment>;
+  return <Fragment key={searchParams?.get('q')}>{children}</Fragment>;
 }
